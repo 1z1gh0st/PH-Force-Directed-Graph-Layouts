29d13066adc6b98a4bb6da77636a5eef10a1bf68
1z1gh0st <wartonp@oregonstate.edu>
2023-12-04 15:09:14 -0800

    Added matplotlib module into window.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from tkinter import *
-
-
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
+
+import numpy as np
+
+from tkinter import *
 
 # TODO:
 #   import graphs
@@ -11,14 +12,21 @@ from matplotlib.figure import Figure
 #   implement kruskals for barcode retrieval
 #   and repulsion and contraction homology UI
 
+# Initialize TKInter Window
 root = Tk()
 root.wm_title("Scientific Visualization Final")
 root.geometry("1280x720")
 
-sliders = Frame()
-
 time_step = 1.0
 
+# Initialize plotting canvas
+fig = Figure(figsize=(5, 4), dpi=100)
+t = np.arange(0, 3, .01)
+fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+plot = FigureCanvasTkAgg(fig, master=root)
+
+# Initialize constant sliders
+sliders = Frame()
 spring_constant = DoubleVar()
 s1 = Scale(
         sliders,
@@ -27,7 +35,6 @@ s1 = Scale(
         to=300,
         orient=HORIZONTAL)
 l1 = Label(sliders, text = "Spring Constant")
-
 Coulumb_constant = DoubleVar()
 s2 = Scale(
         sliders,
@@ -36,7 +43,6 @@ s2 = Scale(
         to=1500,
         orient=HORIZONTAL)
 l2 = Label(sliders, text = "Coulumb Constant")
-
 damping_coefficient = DoubleVar()
 s3 = Scale(
         sliders,
@@ -45,18 +51,19 @@ s3 = Scale(
         to=300,
         orient=HORIZONTAL)
 l3 = Label(sliders, text = "Damping Coefficient")
-
+# Place sliders and labels on grid within sliders module
 l1.grid(row=0, column=0, pady=2)
 s1.grid(row=1, column=0, pady=2)
-
 l2.grid(row=2, column=0, pady=2)
 s2.grid(row=3, column=0, pady=2)
-
 l3.grid(row=4, column=0, pady=2)
 s3.grid(row=5, column=0, pady=2)
 
+homology = Frame(root, bg="blue")
+
+# Arrange main window
+homology.pack(ipadx=10, ipady=10, expand=True, side="bottom", fill="both")
 sliders.pack(ipadx=10, ipady=10, expand=True, side="left", fill="both")
-plot = Canvas(root, bg="green", height=520, width=1080)
-plot.pack(ipadx=10, ipady=10, fill="both")
+plot.get_tk_widget().pack(ipadx=10, ipady=10, fill="both")
 
 root.mainloop()
